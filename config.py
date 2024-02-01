6beb86260a688102927308f09688b7da6eb8c951
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-01 20:24:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
     TOKEN="6572403691:AAFV-3PpCb7N06uqJC2mCdK4QzW6VtV5CeE"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6664582540").split(" "))
-    START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
+    START_IMG="https://telegra.ph/file/3df11bcade4a69a6335f2.jpg"
     BOT_ID=6796545941
     BOT_USERNAME="ZuliaiBot"
     BOT_NAME="ZuliaiBot"
