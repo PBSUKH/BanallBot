cb2244fea37a9643e446e246c5f484a6cc7c8bf9
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-01 20:26:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=24509589
-    API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
+    API_ID=29908998
+    API_HASH="3a0d920a6370b97671ea838d92dc559c"
     TOKEN="6572403691:AAFV-3PpCb7N06uqJC2mCdK4QzW6VtV5CeE"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6664582540").split(" "))
     START_IMG="https://telegra.ph/file/3df11bcade4a69a6335f2.jpg"
-    BOT_ID=6796545941
-    BOT_USERNAME="ZuliaiBot"
-    BOT_NAME="ZuliaiBot"
+    BOT_ID=6146454041
+    BOT_USERNAME="MissBroken_Bot"
+    BOT_NAME="Fucking"
